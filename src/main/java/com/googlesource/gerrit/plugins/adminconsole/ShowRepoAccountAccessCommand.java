e2674057072e74621146bc99a2e77fd0df2dc8e7
David Pursehouse <david.pursehouse@sonymobile.com>
2014-02-07 22:30:34 +0900

    Rename package to com.googlesource.gerrit.plugins.adminconsole
    Change-Id: Iaeebb27c2a64c2c4cc534fa8798c702fb1401b70

--- src/main/java/com/googlesource/gerrit/plugins/adminconsole/ShowRepoAccountAccessCommand.java
+++ src/main/java/com/googlesource/gerrit/plugins/adminconsole/ShowRepoAccountAccessCommand.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.gerrit.plugins;
+package com.googlesource.gerrit.plugins.adminconsole;
 
 import java.io.IOException;
 import java.util.HashSet;
